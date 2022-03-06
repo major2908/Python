a953791b366c4a6c1f81bab7ad0d989226d2cff9
major2908 <101052008+major2908@users.noreply.github.com>
2022-03-06 06:00:08 -0800

    variables

---
+++ class 1.py
@@ -0,0 +1,49 @@
+# 1.#Programe for addition, subraction, multiplication, division
+# n=12
+# m=15
+# sum=n+m
+# sub=n-m
+# mul=n*m
+# div=n/m
+# print(sum, sub, mul, div)
+
+# 2.#Programe for square and cube
+# a=12
+# sqare=a**2
+# cube=a**3
+# print(sqare, cube)
+
+# 3.Programe for area of circle and triangle
+#area of circle
+# pi=3.14
+# r=7
+# area=pi*(r**2)
+# print(area)
+#area of triangle
+# b=5
+# h=6
+# area=1/2*b*h
+# print(area)
+
+# 4.Programe for SWAP
+# n=3
+# m=5
+# n=n+m
+# m=n-m
+# n=n-m
+# print(n,m)
+#alternate method
+
+
+# #5.Programe for total and average
+# a=3
+# b=4
+# c=5
+# total=a+b+c
+# average=total/3
+# print(type(total))
+# print(total,average)
+# print(type(average))
+
+
+
